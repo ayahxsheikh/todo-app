e30e5274a8b9e39677447556d064e119623af30c
cc-aeya <aaminah.x@icloud.com>
2022-12-11 10:41:39 +0000

    keydown working with enter

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,6 @@
 //WHEN ITEM IS SUBMITTED 1:
-//submit prevent default function
-//create ol and li inside showlist function?
-// it needs to show as a list
-// it needs to have a checked feature that when clicked should remove item from list
+//create li inside showlist function
+//needs checked feature that when clicked should remove item from list
 //needs to have a counter of list items
 
 //WHEN ITEM IS SUBMITTED 2:
@@ -10,32 +8,27 @@
 
 //Global Variables:
 var todoInput = $('#todo-input');
-var submitBtn = $('#submit-item'); 
+var output = $('#output');
 var taskCounter = $('#task-no');
-var ulEl = $('#ul');
-var liEl = $('#li');
-var liSpan = $('#liTask')
 var liBtn = $('#complete');
-var todoValue;
+// var todoValue;
 var todoArr = [];
-var taskOutput;
 
-submitBtn.click(function(event){
-    event.preventDefault()
-    console.log('submit worked');
-    // showListItems();
-})
+
 
 //TARGETS INPUT VALUE
-    todoInput.on('change',function(event){
+todoInput.keydown(function(event){
+    var code = event.keyCode;
+    if(code === 13){
         todoValue = event.target.value;
         todoArr.push(todoValue);
-        // value pushed into array;
+        console.log(todoValue);
+    }
         
-        //SAVES INPUT VALUE
+    //SAVES INPUT VALUE
         localStorage.setItem('task', JSON.stringify(todoArr));
         var retrieve = JSON.parse(localStorage.getItem('task'));
-        console.log(retrieve);
+        // console.log(retrieve);
         
     });
 
@@ -46,4 +39,5 @@ submitBtn.click(function(event){
 
 //FUNCTION TO CREATE COUNTER
 // function counter(){}
+
 
